d692a927eef0a3aa33b631b655172e41c0cb51b1
ohyeon <skyblueazure4@gmail.com>
2025-11-27 09:43:13 +0900

    structure arrangement

--- recur.js
+++
@@ -1,6 +0,0 @@
-export default function objTrav(obj) {
-    if(typeof obj === 'object' && obj !== null){
-        return Object.entries(obj).map(([K,V])=>{return {key:K, value:objTrav(V)}});
-    }
-    else return obj;
-}
